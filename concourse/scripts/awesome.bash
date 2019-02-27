2f11f0d73bc191729dc0184269a6b54ef9df5561
Jesse Zhang <sbjesse@gmail.com>
2019-02-26 17:05:10 -0800

    Now you run as gpadmin

--- concourse/scripts/awesome.bash
+++ concourse/scripts/awesome.bash
@@ -4,13 +4,21 @@ set -e -u -o pipefail
 set -x
 
 NCPU=$(getconf _NPROCESSORS_CONF)
+readonly NCPU
+
+: ${USER:=$(whoami)}
+
+make_install() {
+	sudo make -s install "$@"
+}
 
 # be a good neighbor: use all cores when possible, but back off if the worker is loaded
-make_install() {
-	make -s -j ${NCPU} -l $((2 * NCPU)) install "$@"
+make_nicely() {
+	make -s -j ${NCPU} -l $((2 * NCPU)) "$@"
 }
 
 set_up_ccache_env() {
+	# FIXME: is this Debianism? Maybe tell configure (twice) with CC=
 	if [ $(readlink -f $(type -p gcc)) != $(which ccache) ]; then
 		if [ -d /usr/lib/ccache ]; then
 			PATH=/usr/lib/ccache:$PATH
@@ -25,16 +33,19 @@ configure() {
 }
 
 compile() {
+	make_nicely all
 	make_install
 }
 
 fetch_and_build_xerces_c() {
+	# TODO: orca publishing the corresponding xerces with each release
 	git clone https://github.com/greenplum-db/gp-xerces /tmp/xerces
 	(
 		set -e
 		cd /tmp/xerces
 		./configure --disable-network
 	)
+	make_nicely -C /tmp/xerces
 	make_install -C /tmp/xerces
 }
 
@@ -44,14 +55,24 @@ fetch_and_build_orca() {
 	orca_code_url=$(
 		sed -E -n -e '/gporca/s,.*https://github.com/greenplum-db/gporca/releases/download/v(([[:digit:]]|\.)+)/bin_orca_centos5_release.tar.gz.*,https://github.com/greenplum-db/gporca/archive/v\1.tar.gz,p' <gpAux/releng/releng.mk
 	)
-	mkdir /tmp/orca
-	wget -O - "${orca_code_url}" | tar zx --strip-components=1 -C /tmp/orca
+	local ORCA_SRC ORCA_BUILD
+	readonly ORCA_SRC=/tmp/orca
+	readonly ORCA_BUILD=/tmp/orca/build
+	mkdir "${ORCA_SRC}"
+	wget -qO - "${orca_code_url}" | tar zx --strip-components=1 -C "${ORCA_SRC}"
 
-	cmake -GNinja -H/tmp/orca -B/tmp/orca/build
-	ninja install -C /tmp/orca/build
+	cmake -GNinja -H"${ORCA_SRC}" -B"${ORCA_BUILD}"
+	ninja -C "${ORCA_BUILD}"
+	sudo ninja install -C "${ORCA_BUILD}"
+}
+
+workaround_concourse_file_uids() {
+	sudo chown -R "${USER}:${USER}" .
 }
 
 _main() {
+	workaround_concourse_file_uids
+
 	set_up_ccache_env
 
 	fetch_and_build_orca
