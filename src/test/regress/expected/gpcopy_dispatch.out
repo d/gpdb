cloudberrydb/cloudberrydb

commit a8aa1c4acc95ea42af8419a19048023409975659

Only parse enough of each COPY row, to compute which segment to send it to.
In the common case that the distribution key is one of the first columns
in a table, COPY FROM can scale a lot better, if we only parse, and call
the input functions, for the first columns in the QD, and delay parsing
the rest of the into the QEs. The overall effort is the same, but the QD
can easily become a bottleneck.

GPDB 5 did this, but we lost in the refactoring of the COPY code in the
9.1 merge. This brings it back.

Update answer file for test sreh. Master would reject tuple "bad|8|8"
and then second tuple it will see bad will be "eleven". Hence, reject
that. I think previously since master was parsing full tuples it will
see incorrect data for non-distribution columns also. Hence, reported
error for earlier tuple.

Co-authored-by: Ashwin Agrawal <aagrawal@pivotal.io>
Co-authored-by: Melanie Plageman <mplageman@pivotal.io>

src/test/regress/expected/gpcopy_dispatch.out
@@ -1,0 +1,82 @@
+SET test_copy_qd_qe_split = on;
+-- Distributed randomly. QD doesn't need any of the cols.
+CREATE TABLE disttest (a int, b int, c int) DISTRIBUTED RANDOMLY;
+COPY disttest FROM stdin;
+INFO:  first field processed in the QE: 0
+CONTEXT:  COPY disttest, line 0
+DROP TABLE disttest;
+CREATE TABLE disttest (a int, b int, c int) DISTRIBUTED BY (b);
+COPY disttest FROM stdin;
+INFO:  first field processed in the QE: 2
+CONTEXT:  COPY disttest, line 0
+DROP TABLE disttest;
+CREATE TABLE disttest (a int, b int, c int) DISTRIBUTED BY (c);
+COPY disttest FROM stdin;
+INFO:  all fields will be processed in the QD
+CONTEXT:  COPY disttest, line 0
+DROP TABLE disttest;
+CREATE TABLE disttest (a int, b int, c int) DISTRIBUTED BY (c, a);
+COPY disttest FROM stdin;
+INFO:  all fields will be processed in the QD
+CONTEXT:  COPY disttest, line 0
+DROP TABLE disttest;
+-- With column list
+CREATE TABLE disttest (a int, b int, c int) DISTRIBUTED BY (c, b);
+COPY disttest (c, b, a) FROM stdin;
+INFO:  first field processed in the QE: 2
+CONTEXT:  COPY disttest, line 0
+DROP TABLE disttest;
+--
+-- Partitioned scenarios.
+--
+-- Distributed randomly, but QD needs the partitioning key.
+CREATE TABLE partdisttest (a int, b int, c int) DISTRIBUTED RANDOMLY PARTITION BY RANGE (b) (START (1) END (10) EVERY (5));
+NOTICE:  CREATE TABLE will create partition "partdisttest_1_prt_1" for table "partdisttest"
+NOTICE:  CREATE TABLE will create partition "partdisttest_1_prt_2" for table "partdisttest"
+COPY partdisttest FROM stdin;
+INFO:  first field processed in the QE: 2
+CONTEXT:  COPY partdisttest, line 0
+DROP TABLE partdisttest;
+-- With a dropped column
+CREATE TABLE partdisttest (a int, dropped int, b int, c int) DISTRIBUTED RANDOMLY PARTITION BY RANGE (b) (START (1) END (10) EVERY (5));
+ALTER TABLE partdisttest DROP COLUMN dropped;
+COPY partdisttest FROM stdin;
+INFO:  first field processed in the QE: 2
+CONTEXT:  COPY partdisttest, line 0
+DROP TABLE partdisttest;
+-- Hash distributed, with a dropped column
+CREATE TABLE partdisttest (a int, dropped int, b int, c int)
+  DISTRIBUTED BY (b)
+  PARTITION BY RANGE (a) (START (0) END (100) EVERY (50));
+ALTER TABLE partdisttest DROP COLUMN dropped;
+ALTER TABLE partdisttest ADD PARTITION neg start (-10) end (0);
+COPY partdisttest FROM stdin;
+INFO:  first field processed in the QE: 2
+CONTEXT:  COPY partdisttest, line 0
+DROP TABLE partdisttest;
+-- Subpartitions
+CREATE TABLE partdisttest (a int, dropped int, b int, c int, d int)
+  DISTRIBUTED RANDOMLY
+  PARTITION BY RANGE (b)
+  SUBPARTITION BY RANGE (c)
+  (
+    PARTITION b_low start (1)
+    (
+      SUBPARTITION c_low start (1),
+      SUBPARTITION c_hi start (5)
+    ),
+    PARTITION b_hi start (5)
+    (
+      SUBPARTITION c_low start (1),
+      SUBPARTITION c_hi start (5)
+    )
+  );
+ALTER TABLE partdisttest DROP COLUMN dropped;
+COPY partdisttest FROM stdin;
+INFO:  first field processed in the QE: 3
+CONTEXT:  COPY partdisttest, line 0
+ALTER TABLE partdisttest ADD PARTITION b_negative start (-10) end (0) (subpartition c_negative start (-10) end (0));
+COPY partdisttest FROM stdin;
+INFO:  first field processed in the QE: 3
+CONTEXT:  COPY partdisttest, line 0
+DROP TABLE partdisttest;

src/test/regress/sql/gpcopy_dispatch.sql
@@ -1,0 +1,99 @@
+
+SET test_copy_qd_qe_split = on;
+
+-- Distributed randomly. QD doesn't need any of the cols.
+CREATE TABLE disttest (a int, b int, c int) DISTRIBUTED RANDOMLY;
+COPY disttest FROM stdin;
+1	2	3
+\.
+DROP TABLE disttest;
+
+CREATE TABLE disttest (a int, b int, c int) DISTRIBUTED BY (b);
+COPY disttest FROM stdin;
+1	2	3
+\.
+DROP TABLE disttest;
+
+CREATE TABLE disttest (a int, b int, c int) DISTRIBUTED BY (c);
+COPY disttest FROM stdin;
+1	2	3
+\.
+DROP TABLE disttest;
+
+CREATE TABLE disttest (a int, b int, c int) DISTRIBUTED BY (c, a);
+COPY disttest FROM stdin;
+1	2	3
+\.
+DROP TABLE disttest;
+
+-- With column list
+CREATE TABLE disttest (a int, b int, c int) DISTRIBUTED BY (c, b);
+COPY disttest (c, b, a) FROM stdin;
+3	2	1
+\.
+DROP TABLE disttest;
+
+
+--
+-- Partitioned scenarios.
+--
+
+-- Distributed randomly, but QD needs the partitioning key.
+CREATE TABLE partdisttest (a int, b int, c int) DISTRIBUTED RANDOMLY PARTITION BY RANGE (b) (START (1) END (10) EVERY (5));
+COPY partdisttest FROM stdin;
+1	2	3
+\.
+DROP TABLE partdisttest;
+
+-- With a dropped column
+CREATE TABLE partdisttest (a int, dropped int, b int, c int) DISTRIBUTED RANDOMLY PARTITION BY RANGE (b) (START (1) END (10) EVERY (5));
+ALTER TABLE partdisttest DROP COLUMN dropped;
+COPY partdisttest FROM stdin;
+1	2	3
+\.
+DROP TABLE partdisttest;
+
+-- Hash distributed, with a dropped column
+CREATE TABLE partdisttest (a int, dropped int, b int, c int)
+  DISTRIBUTED BY (b)
+  PARTITION BY RANGE (a) (START (0) END (100) EVERY (50));
+ALTER TABLE partdisttest DROP COLUMN dropped;
+
+ALTER TABLE partdisttest ADD PARTITION neg start (-10) end (0);
+
+COPY partdisttest FROM stdin;
+-1	2	3
+\.
+DROP TABLE partdisttest;
+
+
+
+-- Subpartitions
+CREATE TABLE partdisttest (a int, dropped int, b int, c int, d int)
+  DISTRIBUTED RANDOMLY
+  PARTITION BY RANGE (b)
+  SUBPARTITION BY RANGE (c)
+  (
+    PARTITION b_low start (1)
+    (
+      SUBPARTITION c_low start (1),
+      SUBPARTITION c_hi start (5)
+    ),
+    PARTITION b_hi start (5)
+    (
+      SUBPARTITION c_low start (1),
+      SUBPARTITION c_hi start (5)
+    )
+  );
+ALTER TABLE partdisttest DROP COLUMN dropped;
+COPY partdisttest FROM stdin;
+1	2	3	4
+\.
+
+ALTER TABLE partdisttest ADD PARTITION b_negative start (-10) end (0) (subpartition c_negative start (-10) end (0));
+
+COPY partdisttest FROM stdin;
+100	-1	-1	1
+\.
+
+DROP TABLE partdisttest;

src/backend/utils/misc/guc_gp.c
@@ -132,6 +132,7 @@ bool		Debug_appendonly_print_compaction = false;
 bool		Debug_resource_group = false;
 bool		Debug_bitmap_print_insert = false;
 bool		Test_print_direct_dispatch_info = false;
+bool		Test_copy_qd_qe_split = false;
 bool		gp_permit_relation_node_change = false;
 int			gp_max_local_distributed_cache = 1024;
 bool		gp_appendonly_verify_block_checksums = true;
@@ -1501,6 +1502,17 @@ struct config_bool ConfigureNamesBool_gp[] =
 			GUC_SUPERUSER_ONLY | GUC_NO_SHOW_ALL | GUC_NOT_IN_SAMPLE
 		},
 		&Test_print_direct_dispatch_info,
+		false,
+		NULL, NULL, NULL
+	},
+
+	{
+		{"test_copy_qd_qe_split", PGC_SUSET, DEVELOPER_OPTIONS,
+			gettext_noop("For testing purposes, print information about which columns are parsed in QD and which in QE."),
+			NULL,
+			GUC_SUPERUSER_ONLY | GUC_NO_SHOW_ALL | GUC_NOT_IN_SAMPLE
+		},
+		&Test_copy_qd_qe_split,
 		false,
 		NULL, NULL, NULL
 	},

src/include/commands/copy.h
@@ -236,6 +236,9 @@ typedef struct CopyStateData
 
 	/* Greenplum Database specific variables */
 	bool		escape_off;		/* treat backslashes as non-special? */
+	int			first_qe_processed_field;
+	List	   *qd_attnumlist;
+	List	   *qe_attnumlist;
 
 	PartitionNode *partitions; /* partitioning meta data from dispatcher */
 	List		  *ao_segnos;  /* AO table meta data from dispatcher */

src/include/utils/sync_guc_name.h
@@ -110,6 +110,7 @@
 		"statement_mem",
 		"statement_timeout",
 		"temp_buffers",
+		"test_copy_qd_qe_split",
 		"TimeZone",
 		"verify_gpfdists_cert",
 		"vmem_process_interrupt",

src/test/regress/greenplum_schedule
@@ -37,7 +37,7 @@ test: gp_tablespace
 test: temp_tablespaces
 test: default_tablespace
 
-test: leastsquares opr_sanity_gp decode_expr bitmapscan bitmapscan_ao case_gp limit_gp notin percentile join_gp union_gp gpcopy_encoding gp_create_table gp_create_view window_views replication_slots create_table_like_gp gp_constraints matview_ao
+test: leastsquares opr_sanity_gp decode_expr bitmapscan bitmapscan_ao case_gp limit_gp notin percentile join_gp union_gp gpcopy_encoding gp_create_table gp_create_view window_views replication_slots create_table_like_gp gp_constraints matview_ao gpcopy_dispatch
 # below test(s) inject faults so each of them need to be in a separate group
 test: gpcopy
 

src/test/regress/output/sreh.source
@@ -37,8 +37,8 @@ SELECT * FROM sreh_copy ORDER BY a,b,c;
 --
 COPY sreh_copy FROM '@abs_srcdir@/data/bad_data1.data' DELIMITER '|' SEGMENT REJECT LIMIT 2;
 ERROR:  segment reject limit reached, aborting operation
-DETAIL:  Last error was: invalid input syntax for integer: "", column b
-CONTEXT:  COPY sreh_copy, line 8, column a
+DETAIL:  Last error was: invalid input syntax for integer: "eleven", column a
+CONTEXT:  COPY sreh_copy, line 11, column a: "eleven"
 SELECT * FROM sreh_copy ORDER BY a,b,c;
  a  | b  | c  
 ----+----+----

d/gpdb

commit 6fda144f1604306f08ed757447ab87791b25e8fd

COPY: Simplify partition distribution policy lookup
Commit a8aa1c4acc95ea42 introduced a bug when a partition has a
different distribution policy (in terms of column numbers) from the base
table: we would perform a partition lookup -- using a tuple table slot
for the partition but (incorrectly) with a tuple descriptor for the base
table. This would sometimes lead to an error of "no partition for
partitioning key". Upon closer inspection, we didn't even need to look
up the partition because the caller already knew!

This commit fixes that, and simplifies the logic in
GetDistributionPolicyForPartition to just take a resultRelInfo instead
of the tuple values.

Co-authored-by: Jesse Zhang <sbjesse@gmail.com>
Co-authored-by: Ashwin Agrawal <aagrawal@pivotal.io>

src/backend/commands/copy.c
@@ -224,11 +224,9 @@ static GpDistributionData *InitDistributionData(CopyState cstate, EState *estate
 static void FreeDistributionData(GpDistributionData *distData);
 static void InitCopyFromDispatchSplit(CopyState cstate, GpDistributionData *distData, EState *estate);
 static Bitmapset *GetTargetKeyCols(Oid relid, PartitionNode *children, Bitmapset *needed_cols, bool distkeys, EState *estate);
-static GpDistributionData *GetDistributionPolicyForPartition(CopyState cstate,
-								  EState *estate,
-								  GpDistributionData *mainDistData,
-								  TupleDesc tupDesc,
-								  Datum *values, bool *nulls);
+static GpDistributionData *GetDistributionPolicyForPartition(GpDistributionData *mainDistData,
+															 ResultRelInfo *resultRelInfo,
+															 MemoryContext context);
 static unsigned int
 GetTargetSeg(GpDistributionData *distData, Datum *baseValues, bool *baseNulls);
 static ProgramPipes *open_program_pipes(char *command, bool forwrite);
@@ -4052,21 +4050,22 @@ CopyFrom(CopyState cstate)
 			{
 				/* In QD, compute the target segment to send this row to. */
 				part_distData = GetDistributionPolicyForPartition(
-					cstate, estate,
-					distData,
-					tupDesc,
-					slot_get_values(slot), slot_get_isnull(slot));
+																  distData,
+																  resultRelInfo,
+																  cstate->copycontext);
 
 				target_seg = GetTargetSeg(part_distData, slot_get_values(slot), slot_get_isnull(slot));
 			}
 			else if (is_check_distkey)
 			{
-				/* In COPY FROM ON SEGMENT, check the distribution key in the QE. */
+				/*
+				 * In COPY FROM ON SEGMENT, check the distribution key in the
+				 * QE.
+				 */
 				part_distData = GetDistributionPolicyForPartition(
-					cstate, estate,
-					distData,
-					tupDesc,
-					slot_get_values(slot), slot_get_isnull(slot));
+																  distData,
+																  resultRelInfo,
+																  cstate->copycontext);
 
 				if (part_distData->policy->nattrs != 0)
 				{
@@ -7807,10 +7806,9 @@ GetTargetKeyCols(Oid relid, PartitionNode *children, Bitmapset *needed_cols,
 
 /* Get distribution policy for specific part */
 static GpDistributionData *
-GetDistributionPolicyForPartition(CopyState cstate, EState *estate,
-								  GpDistributionData *mainDistData,
-                                  TupleDesc tupDesc,
-                                  Datum *values, bool *nulls)
+GetDistributionPolicyForPartition(GpDistributionData *mainDistData,
+								  ResultRelInfo *resultRelInfo,
+								  MemoryContext context)
 {
 
 	/*
@@ -7821,27 +7819,21 @@ GetDistributionPolicyForPartition(CopyState cstate, EState *estate,
 	if (mainDistData->hashmap)
 	{
 		Oid			relid;
-		ResultRelInfo *resultRelInfo;
 		GpDistributionData *d;
 		bool		found;
 
-		resultRelInfo = values_get_partition(values,
-											 nulls,
-											 tupDesc,
-											 estate,
-											 false); /* don't need indices in QD */
 		relid = resultRelInfo->ri_RelationDesc->rd_id;
 
 		d = hash_search(mainDistData->hashmap, &(relid), HASH_ENTER, &found);
 		if (!found)
 		{
-			Relation rel = resultRelInfo->ri_RelationDesc;
+			Relation	rel = resultRelInfo->ri_RelationDesc;
 			MemoryContext oldcontext;
 
 			/*
 			 * Make sure this all persists the current iteration.
 			 */
-			oldcontext = MemoryContextSwitchTo(cstate->copycontext);
+			oldcontext = MemoryContextSwitchTo(context);
 
 			d->cdbHash = makeCdbHashForRelation(rel);
 			d->policy = GpPolicyCopy(rel->rd_cdbpolicy);

src/test/regress/expected/gpcopy_dispatch.out
@@ -45,14 +45,28 @@ INFO:  first field processed in the QE: 2
 CONTEXT:  COPY partdisttest, line 0
 DROP TABLE partdisttest;
 -- Hash distributed, with a dropped column
-CREATE TABLE partdisttest (a int, dropped int, b int, c int)
-  DISTRIBUTED BY (b)
-  PARTITION BY RANGE (a) (START (0) END (100) EVERY (50));
-ALTER TABLE partdisttest DROP COLUMN dropped;
+-- We used to have a bug where QD would pick the wrong partition and/or the
+-- wrong segment due to difference between the base table and a partition: the
+-- partitioing attribute(s) for the root table is column 3, but it is column 1
+-- in the leaf partition "neg". The QD would then mistakenly pick a partition
+-- for the NULL value, and error out that no such a partition exists.
+-- Note if the dropped columns are in a different position, a different (but
+-- really similar) symptom will appear: the QD will pick another partition,
+-- which potentially results in the wrong segment receiving the line / tuple.
+CREATE TABLE partdisttest (dropped1 int, dropped2 int, a int, b int, c int)
+  DISTRIBUTED BY (a)
+  PARTITION BY RANGE (b) (START (0) END (100) EVERY (50));
+ALTER TABLE partdisttest DROP COLUMN dropped1, DROP COLUMN dropped2;
 ALTER TABLE partdisttest ADD PARTITION neg start (-10) end (0);
 COPY partdisttest FROM stdin;
 INFO:  first field processed in the QE: 2
 CONTEXT:  COPY partdisttest, line 0
+SELECT tableoid::regclass, * FROM partdisttest;
+        tableoid        | a | b  | c 
+------------------------+---+----+---
+ partdisttest_1_prt_neg | 2 | -1 | 3
+(1 row)
+
 DROP TABLE partdisttest;
 -- Subpartitions
 CREATE TABLE partdisttest (a int, dropped int, b int, c int, d int)

src/test/regress/sql/gpcopy_dispatch.sql
@@ -54,16 +54,29 @@ COPY partdisttest FROM stdin;
 DROP TABLE partdisttest;
 
 -- Hash distributed, with a dropped column
-CREATE TABLE partdisttest (a int, dropped int, b int, c int)
-  DISTRIBUTED BY (b)
-  PARTITION BY RANGE (a) (START (0) END (100) EVERY (50));
-ALTER TABLE partdisttest DROP COLUMN dropped;
+
+-- We used to have a bug where QD would pick the wrong partition and/or the
+-- wrong segment due to difference between the base table and a partition: the
+-- partitioing attribute(s) for the root table is column 3, but it is column 1
+-- in the leaf partition "neg". The QD would then mistakenly pick a partition
+-- for the NULL value, and error out that no such a partition exists.
+
+-- Note if the dropped columns are in a different position, a different (but
+-- really similar) symptom will appear: the QD will pick another partition,
+-- which potentially results in the wrong segment receiving the line / tuple.
+
+CREATE TABLE partdisttest (dropped1 int, dropped2 int, a int, b int, c int)
+  DISTRIBUTED BY (a)
+  PARTITION BY RANGE (b) (START (0) END (100) EVERY (50));
+ALTER TABLE partdisttest DROP COLUMN dropped1, DROP COLUMN dropped2;
 
 ALTER TABLE partdisttest ADD PARTITION neg start (-10) end (0);
 
 COPY partdisttest FROM stdin;
--1	2	3
+2	-1	3
 \.
+
+SELECT tableoid::regclass, * FROM partdisttest;
 DROP TABLE partdisttest;
 
 