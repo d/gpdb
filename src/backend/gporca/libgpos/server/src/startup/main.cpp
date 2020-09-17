cd8406ba49c4bcaf37378aca7186bfda5ce276b8
Heikki Linnakangas <hlinnakangas@pivotal.io>
2020-09-17 15:35:17 +0300

    Remove obsolete tests for "custom allocators".
    The concept of custom allocates was removed in commit 6efbd65161, but
    these tests were left behind. The gpos_test program ignored the -c option,
    so we ran the CMemoryPoolBasicTest and CCacheTest twice, with no
    difference.

--- src/backend/gporca/libgpos/server/src/startup/main.cpp
+++ src/backend/gporca/libgpos/server/src/startup/main.cpp
@@ -151,7 +151,7 @@ INT main
 	)
 {	
 	// setup args for unittest params
-	CMainArgs ma(iArgs, rgszArgs, "cuU:xT:");
+	CMainArgs ma(iArgs, rgszArgs, "uU:xT:");
 
 	struct gpos_init_params init_params = { NULL };
 	gpos_init(&init_params);
